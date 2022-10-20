ba0091bb5d3af56c57d8d6be14fbb3ef5fadb78f
Apurv gupta <56751664+apurvgupta-ops@users.noreply.github.com>
2022-10-21 03:32:25 +0530

    slider

--- src/Components/HotelDetail.js
+++ src/Components/HotelDetail.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsArrowLeftSquareFill } from "react-icons/bs";
+import { BsArrowRightSquareFill } from "react-icons/bs";
+import { FaWindowClose } from "react-icons/fa";
 import pic1 from "../Image/pic 1.jpg";
 import pic2 from "../Image/pic 2.jpg";
 import pic3 from "../Image/pic 3.jpg";
@@ -16,10 +19,27 @@ const images = [
 ];
 
 const HotelHotelDetail = () => {
+  const [sliderNo, setSliderNo] = useState(0);
+  const [openSlider, setOpenSlider] = useState(false);
+  const handleOpen = (i) => {
+    setSliderNo(i);
+    setOpenSlider(true);
+  };
+
+  const handleMove = (direction) => {
+    let newSlideNo;
+    if (direction === "l") {
+      newSlideNo = sliderNo === 0 ? 5 : sliderNo - 1;
+    } else {
+      newSlideNo = sliderNo === 5 ? 0 : sliderNo + 1;
+    }
+
+    setSliderNo(newSlideNo);
+  };
   return (
     <>
-      <div className="w-full mt-6">
-        <div className="flex justify-between w-full">
+      <div className="w-full mt-6 ">
+        <div className="flex justify-between w-full relative">
           <h1 className="font-bold text-3xl ">Neemrana Hotel</h1>
           <button className="bg-[#3287fd] px-2 rounded text-white">
             Reserve or Book Now!
@@ -34,9 +54,36 @@ const HotelHotelDetail = () => {
             Book a stay over 5000 at this property and get a free airport texi
           </p>
         </div>
+        {openSlider && (
+          <div className="w-[80%] bg-gray-50 bg-opacity-70 h-[80%] z-50 flex items-center absolute">
+            <FaWindowClose
+              size={30}
+              className="absolute top-[20px] right-[20px] cursor-pointer"
+              onClick={() => setOpenSlider(false)}
+            />
+            <BsArrowLeftSquareFill
+              size={30}
+              className="m-5 cursor-pointer"
+              onClick={() => handleMove("l")}
+            />
+            <div className="w-full h-full flex items-center justify-center">
+              <img src={images[sliderNo].image} alt="" className="w-[60%] " />
+            </div>
+            <BsArrowRightSquareFill
+              size={30}
+              className="m-5 cursor-pointer"
+              onClick={() => handleMove("r")}
+            />
+          </div>
+        )}
         <div className="flex gap-2 flex-wrap w-full mt-4 mb-6">
-          {images.map((image) => (
-            <img src={image.image} alt="" className="w-[399px]" />
+          {images.map((image, i) => (
+            <img
+              src={image.image}
+              alt=""
+              className="w-[399px]"
+              onClick={() => handleOpen(i)}
+            />
           ))}
         </div>
         <div className="mb-8">
