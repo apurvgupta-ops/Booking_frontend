84b3f5e22db1140d46363d39eb921d5fd83acc07
Apurv gupta <56751664+apurvgupta-ops@users.noreply.github.com>
2022-10-31 23:28:17 +0530

    Navigation

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex justify-between items-center pt-4">
-      <div className="text-white text-2xl font-bold ">Booking</div>
+      <div
+        className="text-white text-2xl font-bold cursor-pointer"
+        onClick={() => navigate("/")}
+      >
+        Booking
+      </div>
       <div className="">
         <button className=" p-1 mr-4 bg-white text-[#003580]">Register</button>
         <button className=" p-1  bg-white text-[#003580]">Signin</button>
